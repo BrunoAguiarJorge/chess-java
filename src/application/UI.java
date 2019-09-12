1e0958c13146e2721e2bd1f3e9800162ab7c28af
Bruno Aguiar <brunoaguiar@Brunos-MacBook-Air.local>
2019-09-12 19:25:31 +0100

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -54,9 +54,11 @@ public class UI {
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
-
 		System.out.println("Turn: " + chessMatch.getCurrentPlayer());
 		System.out.println(" Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
