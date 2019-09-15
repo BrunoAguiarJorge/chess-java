de18637dc327293be49084e32694e9f95310dc4d
Bruno Aguiar <brunoaguiar@Brunos-MacBook-Air.local>
2019-09-15 19:20:50 +0100

    Passant and promotion

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,9 +1,10 @@
 package boardgame;
 
 public abstract class Piece {
+
 	protected Position position;
 	private Board board;
-
+	
 	public Piece(Board board) {
 		this.board = board;
 		position = null;
@@ -13,7 +14,7 @@ public abstract class Piece {
 		return board;
 	}
 	
-	public abstract boolean [][] possibleMoves();
+	public abstract boolean[][] possibleMoves();
 	
 	public boolean possibleMove(Position position) {
 		return possibleMoves()[position.getRow()][position.getColumn()];
@@ -29,6 +30,5 @@ public abstract class Piece {
 			}
 		}
 		return false;
-		
 	}
 }
